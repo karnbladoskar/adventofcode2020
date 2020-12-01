fa5ee6b7eca0e665f59fed2848adb2539f13bf7c
Oskar Karnblad <oskar.karnblad@gmail.com>
2020-12-01 16:44:22 +0100

    Part2 solved

--- 1/index.js
+++ 1/index.js
@@ -20,17 +20,39 @@ function readLocalTxt(path) {
   });
 }
 
+// PART 1:
+// readLocalTxt("./data.txt").then((res) => {
+//   let Y = 2020;
+//   let rest = 0;
+
+//   res.forEach((el, idx) => {
+//     rest = Y - el;
+//     let X = res.slice(idx);
+
+//     if (X.includes(rest)) {
+//       console.log(rest, el, rest * el);
+//       return;
+//     }
+//   });
+// });
+
+// PART 2:
 readLocalTxt("./data.txt").then((res) => {
   let Y = 2020;
   let rest = 0;
 
-  res.forEach((el, idx) => {
-    rest = Y - el;
-    let X = res.slice(idx);
+  res.forEach((el1, idx1) => {
+    let X = res.slice(idx1);
 
-    if (X.includes(rest)) {
-      console.log(rest, el, rest * el);
-      return;
-    }
+    X.forEach((el2, idx2) => {
+      //   console.log("INDEX 1: ", idx1, " INDEX 2: ", idx2);
+      let Z = X.slice(idx2);
+
+      rest = Y - el1 - el2;
+      if (Z.includes(rest) && (el1 !== el2) !== rest) {
+        console.log(rest, el1, el2, rest * el1 * el2);
+        return;
+      }
+    });
   });
 });
